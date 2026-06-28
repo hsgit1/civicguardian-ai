cb409ccc8d92db21c32b2d561610a12b523aa244
hsgit1 <155060790+hsgit1@users.noreply.github.com>
2026-06-28 16:51:08 +0000

    Fix Leaflet marker icons

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,14 @@ import L from "leaflet";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+const markerIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
 });
 
 const aiClient = new GoogleGenAI({
@@ -686,6 +687,7 @@ export default function App() {
                     <Marker
                       key={issue.id}
                       position={[issue.lat || 22.5726, issue.lng || 88.3639]}
+                      icon={markerIcon}
                     >
                       <Popup>
                         <b>{issue.title}</b>
